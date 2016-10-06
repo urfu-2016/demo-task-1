6dd1f4f9853e2e5c549e44443caf184e396b7f72
Шмакова Мария <shmakovamaria95@gmail.com>
2016-10-07 00:27:16 +0500

    Исправленный в оформлении первый вариант

--- math.js
+++ math.js
@@ -3,8 +3,9 @@
 exports.isStar = false;
 
 exports.sum = function (a, b) {
-	a=Number(a);
-	b=Number(b);
-	return a+b;
+    a = Number(a);
+    b = Number(b);
+	
+    return a + b;
     // Реализуйте сложение в этой функции
 };
