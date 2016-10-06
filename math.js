021ad91ef2f5ecfa930ec6f2ff60aa6c4ef34040
KatyrevaElizaveta <alchimia@ro.ru>
2016-10-06 21:33:13 +0600

    Bug fix 2

--- math.js
+++ math.js
@@ -2,8 +2,14 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b, c) {
-    c = c || 0;
+exports.sum = function (a, b, cOptional) {
+    var c;
+    if (cOptional === undefined) {
+        c = 0;
+    } else {
+        c = cOptional;
+    }
+
     if (!isNaN(parseInt(a)) && !isNaN(parseInt(b)) && !isNaN(parseInt(c))) {
         return a + b + c;
     }
