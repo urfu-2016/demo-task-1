d42af2f0f9fa351d0368bdcde239e34f93e44422
Смирнов Алексей <79068122307@yandex.ru>
2016-10-07 18:02:33 +0500

    Добавил пробел

--- math.js
+++ math.js
@@ -6,6 +6,7 @@ exports.sum = function (a, b, c) {
     if (arguments.length === 2) {
         return (parseInt(a)) + b;
     }
+    
     return (parseInt(a)) + b + c;
     // Реализуйте сложение в этой функции
 };
