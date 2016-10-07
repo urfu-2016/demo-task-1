41ac72bf58d3d7a9f8831f744f8dcc8065ade9ec
Nikolai Bravilov <bravilov@mail.ru>
2016-10-07 19:43:35 +0500

    Правильное суммирование со строками

--- math.js
+++ math.js
@@ -2,6 +2,19 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    return a + b;
+exports.sum = function (a, b, c) {
+    a = parseInt(a);
+    b = parseInt(b);
+    if (isNaN(a) || isNaN(b)) {
+        return -1;
+    }
+    if (c == undefined) {
+        return a + b;
+    } else {
+        c = parseInt(c);
+        if (isNaN(c)) {
+            return -1;
+        }
+        return 0 + a + b + c;
+    }
 };
