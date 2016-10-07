b11e58b26fc52a8852452bfb5bbecf6dcb8dbaec
askeip <paparazzi730@gmail.com>
2016-10-07 16:03:43 +0500

    brace-style, block-spacing, no-trailing-spaces updated

--- math.js
+++ math.js
@@ -4,16 +4,12 @@ exports.isStar = true;
 
 exports.sum = function () {
     var sum = parseInt(arguments[0]);
-    if (isNaN(sum)) {
-        return NaN;}
-    for (var i = 1; i < arguments.length; i++)
-    {
+    if (isNaN(sum)) return NaN;
+    for (var i = 1; i < arguments.length; i++) {
         var num = parseInt(arguments[i]);
-        if (!isNaN(num)) {
-            sum += num;}
-        else {
-            return NaN;}
+        if (!isNaN(num)) sum += num;
+        else return NaN;
     }
-    
+
     return sum;
 };
