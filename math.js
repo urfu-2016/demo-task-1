597e2800a6f0fe94a713b9df997878e014345316
Николаев Вадим <nikolayevvadim@gmail.com>
2016-10-07 12:50:51 +0500

    eslint complete

--- math.js
+++ math.js
@@ -3,8 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-	if (c==undefined) {
-		c = 0;
-	}
-    return parseInt(a)+parseInt(b)+parseInt(c);
+    if (c === undefined) {
+        c = 0;
+    }
+
+    return parseInt(a) + parseInt(b) + parseInt(c);
 };
