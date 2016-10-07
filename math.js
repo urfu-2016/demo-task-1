02eb98aa2d81d46a9062d804fc00d7e911cc8482
skobyakov <sergeykob96@gmail.com>
2016-10-07 13:14:10 +0500

    sum of three

--- math.js
+++ math.js
@@ -5,3 +5,7 @@ exports.isStar = true;
 exports.sum = function (a, b) {
     return Number(a) + Number(b);
 };
+
+exports.sumOfThree = function (a, b, c) {
+    return Number(a) + Number(b) + Number(c);
+};
