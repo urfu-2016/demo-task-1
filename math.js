aacc0d5dbabf6e0c95bc593bed7abd650bc98c6d
ADruzhinin <mcfriend@yandex.ru>
2016-10-07 19:49:26 +0500

    Extra 1

--- math.js
+++ math.js
@@ -1,7 +1,11 @@
 'use strict';
 
-exports.isStar = false;
+exports.isStar = true;
 
-exports.sum = function (a, b) {
-    return parseInt(a) + parseInt(b);
+exports.sum = function (a, b, c) {
+    if (c === undefined) {
+        return parseInt(a) + parseInt(b);
+    } else {
+        return parseInt(a) + parseInt(b) + parseInt(c)
+    }
 };
