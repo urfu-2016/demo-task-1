6a39769aee32f2cd6be64394f89292183195bcab
Pavel <pp-1995@mail.ru>
2016-10-07 22:11:06 +0500

    Сложил два числа

--- math.js
+++ math.js
@@ -1,7 +1,8 @@
 'use strict';
-
-exports.isStar = true;
-
+exports.isStar = false;
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
+    var firstNumber = parseInt(a),
+        secondNumber = parseInt(b);
+    return firstNumber + secondNumber;
 };
