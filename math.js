a153c9929072911f754872423a95c5d0e79b0124
Pavel <pp-1995@mail.ru>
2016-10-07 22:43:08 +0500

    Исправил баг связанный с линтингом

--- math.js
+++ math.js
@@ -1,8 +1,9 @@
 'use strict';
 exports.isStar = false;
 exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
-    var firstNumber = parseInt(a),
-        secondNumber = parseInt(b);
-    return firstNumber + secondNumber;
+    var firstNumber = parseInt(a);
+    var secondNumber = parseInt(b);
+    var result = firstNumber + secondNumber;
+
+    return result;
 };
