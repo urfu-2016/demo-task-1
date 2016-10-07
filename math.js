3e070230240b0842cc152f79b868f576550d7ca5
alexanderkine <alexanderkine@users.noreply.github.com>
2016-10-07 23:52:18 +0600

    Корректировки 5

--- math.js
+++ math.js
@@ -5,7 +5,6 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     if (c === undefined) {
         return a + b;
-    } else {
-        return a + b + c;
-    }
+        
+    return a + b + c;
 };
