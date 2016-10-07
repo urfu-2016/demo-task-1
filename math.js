738d296968ede6934287b81a5c19dc87e0d7f6d3
askeip <paparazzi730@gmail.com>
2016-10-07 16:08:38 +0500

    curly fixed

--- math.js
+++ math.js
@@ -4,11 +4,11 @@ exports.isStar = true;
 
 exports.sum = function () {
     var sum = parseInt(arguments[0]);
-    if (isNaN(sum)) return NaN;
+    if (isNaN(sum)) { return NaN; }
     for (var i = 1; i < arguments.length; i++) {
         var num = parseInt(arguments[i]);
-        if (!isNaN(num)) sum += num;
-        else return NaN;
+        if (!isNaN(num)) { sum += num; }
+        else { return NaN; }
     }
 
     return sum;
