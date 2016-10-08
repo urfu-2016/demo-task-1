57c29ae21a3d4618fdcd4bef2352e78f2594e61e
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2016-10-08 14:14:11 +0500

    Второй вариант решения

--- math.js
+++ math.js
@@ -1,8 +1,20 @@
 'use strict';
 
 exports.isStar = false;
-
+function typeChecking (c, d){
+    if ((typeof d == 'string')){
+            var d1 = Number(d);
+            return c+d1;
+        } else {
+            return c+d;
+        }
+}
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
-    return a+b;
+    if ((typeof a) === 'string'){
+        var a1 = Number(a);
+        return typeChecking(a1,b);
+    } else {
+        return typeChecking(a,b);
+    }
 };
