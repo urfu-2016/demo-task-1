8347d54946fa153222040ea28a02d2527d7d407c
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2016-10-08 15:23:12 +0500

    Исправленный систаксис

--- math.js
+++ math.js
@@ -1,20 +1,24 @@
 'use strict';
 
 exports.isStar = false;
-function typeChecking (c, d){
-    if ((typeof d == 'string')){
-            var d1 = Number(d);
-            return c+d1;
-        } else {
-            return c+d;
-        }
+function typeChecking(c, d) {
+	var ans = 0;
+    if ((typeof d) === 'string') {
+        var d1 = Number(d);
+        ans = c + d1;
+    } else {
+        ans = c + d;
+    }
+	return ans;
 }
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
-    if ((typeof a) === 'string'){
+	var answer = 0;
+    if ((typeof a) === 'string') {
         var a1 = Number(a);
-        return typeChecking(a1,b);
+        answer = typeChecking(a1, b);
     } else {
-        return typeChecking(a,b);
+        answer = typeChecking(a, b);
     }
+	return answer;
 };
