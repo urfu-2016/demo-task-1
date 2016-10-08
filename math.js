7c7b5447605b229337ba4966e03f09851d492bd0
Лера <onahochetnamars@yandex.ru>
2016-10-09 02:52:02 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,6 +3,7 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
-    if (isNaN(a) && isNaN(b)) return NaN; else return a+b;
+    if (isNaN(a) && isNaN(b)) 
+        return NaN; 
+    else return a+b;
 };
