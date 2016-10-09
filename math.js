e962cb951d523da68a4eeb0ddcc905b00144e6bb
chrighter <christina.ilyenko@yandex.ru>
2016-10-09 14:10:46 +0600

    Update math.js

--- math.js
+++ math.js
@@ -4,9 +4,8 @@ exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
-    if (typeof a !== 'string' && typeof b !== 'string') {
-        return a + b;
-    } else {
+    if (typeof a === 'string' && typeof b === 'string') {
         throw new TypeError();
     }
+    return a + b;
 };
