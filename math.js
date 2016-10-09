dd05e73dafadd8157d36451d837f5d6b645889a2
alexanderkine <alexanderkine@users.noreply.github.com>
2016-10-09 18:27:09 +0600

    Update math.js

--- math.js
+++ math.js
@@ -5,6 +5,7 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     if (c === undefined) {
         return parseInt(a) + parseInt(b);
-        
+    }
+    
     return parseInt(a) + parseInt(b) + parseInt(c);
 };
