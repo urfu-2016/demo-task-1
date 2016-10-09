6ec4aa29cda5d94ae89411ed53dadfc7b818f023
VeraBasko <vr.basko@gmail.com>
2016-10-10 00:43:36 +0500

    Добавлен первый вариант решения

--- math.js
+++ math.js
@@ -4,4 +4,9 @@ exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
+    return a + b;
+};
+
+export.sum = function(a, b, c) {
+    return a + b + c;
 };
