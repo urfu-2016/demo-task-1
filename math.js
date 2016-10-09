c0c31f720c320bc798d4095691ac266955d0c449
Черных Артём <chernykhand@mail.ru>
2016-10-10 00:02:31 +0500

    update

--- math.js
+++ math.js
@@ -4,5 +4,8 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
+    if (!c){
+        c = 0;
+    }
     return Number(a) + Number(b) + Number(c);
 };
