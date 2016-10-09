ee8cfb640ed61887479128c167e3f5abdb21a093
chrighter <christina.ilyenko@yandex.ru>
2016-10-09 14:26:26 +0600

    Update math.js

--- math.js
+++ math.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
-    if (typeof a !== 'number'  && typeof b !== 'number') {
+    if (typeof a !== "number" && typeof b !== "number") {
         throw new TypeError();
     }
 
