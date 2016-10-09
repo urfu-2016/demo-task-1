e9896745cb0f81bffdc3473bfcdbd3942e130772
Zeace <pern2500@ya.ru>
2016-10-09 11:02:32 +0500

    исправил оформление (3)

--- math.js
+++ math.js
@@ -6,10 +6,10 @@ exports.sum = function (a, b, c) {
     var x = 0;
     if (c === undefined) {
         x = parseInt(a, 10) + parseInt(b, 10);
-    }
-    else {
+    } else {
         x = parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
     }
+
     return x;
 };
 
