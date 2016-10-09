53a0bd9eb14e75dfdf7a227f60d97b3ce179eaf7
VeraBasko <vr.basko@gmail.com>
2016-10-10 00:54:53 +0500

    Второй вариант решения задачи

--- math.js
+++ math.js
@@ -1,12 +1,8 @@
 'use strict';
 
-exports.isStar = true;
+exports.isStar = false;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
     return a + b;
-};
-
-export.sum = function(a, b, c) {
-    return a + b + c;
 };
