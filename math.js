30fe2637b300002ec3b578693c223174e16d781f
Artem <Artem>
2016-10-09 23:45:05 +0500

    addition numbers

--- math.js
+++ math.js
@@ -2,6 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
+exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
+    return Number(a) + Number(b) + Number(c);
 };
