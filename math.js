041b83cfab69b8d50932c2dfb175a5ade6ed1ece
Zeace <pern2500@ya.ru>
2016-10-09 10:46:59 +0500

    исправил оформление

--- math.js
+++ math.js
@@ -3,8 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    if (c===undefined) {return parseInt(a,10)+parseInt(b,10);}
-    else {return parseInt(a,10)+parseInt(b,10)+parseInt(c,10);}// Реализуйте сложение в этой функции
-
+    if (c === undefined) {
+        return parseInt(a, 10) + parseInt(b, 10);
+    }
+    else return parseInt(a, 10)+parseInt(b, 10) + parseInt(c, 10);
 };
 
