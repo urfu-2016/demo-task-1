e73076ce26fc0e89c9d0b697f2853d8f6596a7d4
ByzovGeorgy <sonissd@gmail.com>
2016-10-09 16:58:01 +0500

    add sum three numbers

--- math.js
+++ math.js
@@ -2,6 +2,12 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    return parseInt(a) + parseInt(b);
+exports.sum = function (a, b, c) {
+    var sum = 0;
+    for (var i = 0; i < 3 && i < arguments.length; i++) {
+        if (arguments[i] != undefined) {
+            sum += parseInt(arguments[i]);
+        }
+    }
+    return sum;
 };
