64026554770c743a58216bd1c21041a51f27db0e
Shmarova Elena <shmarova.ea@gmail.ru>
2016-10-09 21:59:43 +0500

    Мое решение задачи

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 var sum = require('./math').sum;
 
-console.info(sum(1, 2));
+console.info(sum(1, 2, 7));
 // 3
 
-console.info(sum(63, 15));
+console.info(sum(63, 15, '1'));
 // 78
 
-console.info(sum('12', 7));
+console.info(sum('12', 7, 3));
 // 19
--- math.js
+++ math.js
@@ -2,6 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
+exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
+    return(parseInt(a)+parseInt(b)+parseInt(c));
 };
