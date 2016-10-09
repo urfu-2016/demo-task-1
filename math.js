61ef5ebe9b1eb1c1661b038786594babd7304c3d
chrighter <christina.ilyenko@yandex.ru>
2016-10-09 15:12:08 +0600

    Update math.js

--- math.js
+++ math.js
@@ -4,8 +4,9 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
-    if (typeof c !== "undefined") {
+    if (c === "undefined") {
         return parseInt(a, 10) + parseInt(b, 10);
     }
+
     return parseInt(a, 10) + parseInt(b, 10) + parseInt(c, 10);
 };
