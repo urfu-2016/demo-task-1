32307b1eba73da286ababd83d043ff69cee22455
nest-aka-swan <swan.nikita@gmail.com>
2016-10-10 10:57:07 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,7 +3,7 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    var sum = a + b;
+    var sum = parseInt(a) + parseInt(b);
 
-    return arguments[2] ? sum + arguments[2] : sum;
+    return arguments[2] ? sum + parseInt(arguments[2]) : sum;
 };
