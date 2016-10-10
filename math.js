3a5625fbf0e34adfbb477211ea8fc6818afc1c62
Кулаев Игорь <kulaevmath@gmail.com>
2016-10-10 10:01:13 +0500

    Добавлен второй вариант решения

--- math.js
+++ math.js
@@ -2,6 +2,9 @@
 
 exports.isStar = false;
 
-exports.sum = function (a, b) {
-    return a + b;
+exports.sum = function (num1, num2) {
+	if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+		throw new TypeError("Expected type is number");
+	}
+    return num1 + num2;
 };
