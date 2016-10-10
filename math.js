b4714b8cd1fd9ec2791efc13f6a32512918b932f
Алексей Котельников <alexeykotelnikov4@gmail.com>
2016-10-11 01:27:09 +0500

    fixed bugs

--- math.js
+++ math.js
@@ -5,7 +5,8 @@ exports.isStar = true;
 exports.sum = function (number1, number2, number3) {
     number1 = parseInt(number1);
     number2 = parseInt(number2);
-    number3 = parseInt(number3);
-
-    return number1 + number2 + number3;
+    if (number3 === undefined);
+        return number1 + number2;
+    else
+        return number1 + number2 + number3;
 };
