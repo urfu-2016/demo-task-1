11e35e1d55fdf5846e2b859220f962fa8a21c615
Irina <irinachkas.96@gmail.com>
2016-10-10 23:53:06 +0500

    add space

--- math.js
+++ math.js
@@ -4,7 +4,7 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     if (c === undefined) {
-	    c = 0;
+        c = 0;
     }
     
     return parseInt(a) + parseInt(b) + parseInt(c);
