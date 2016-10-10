b5f987ba4c6060891ca51096d906842465ef8739
Кулаев Игорь <kulaevmath@gmail.com>
2016-10-10 10:11:39 +0500

    Добавлен второй вариант решения

--- math.js
+++ math.js
@@ -3,8 +3,8 @@
 exports.isStar = false;
 
 exports.sum = function (num1, num2) {
-	if (typeof(num1) !== "number" || typeof(num2) !== "number") {
-		throw new TypeError("Expected type is number");
-	}
-    return num1 + num2;
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        throw new TypeError("Expected type is number");
+}
+return num1 + num2;
 };
