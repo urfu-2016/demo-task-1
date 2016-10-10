e36843cecb286ec8df0859111d02362a1c292f93
Irina <irinachkas.96@gmail.com>
2016-10-10 23:44:47 +0500

    change tub

--- math.js
+++ math.js
@@ -3,8 +3,8 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-	if (c === undefined){
-		c=0;
-	}
+    if (c === undefined){
+	    c=0;
+    }
     return parseInt(a)+parseInt(b);
 };
