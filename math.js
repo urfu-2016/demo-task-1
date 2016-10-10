0faf4b45c386cf5c95522486c1e70497e4aff713
Artem Borzov <artem.borz@yandex.ru>
2016-10-10 23:26:56 +0500

    try 3

--- math.js
+++ math.js
@@ -3,8 +3,6 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    if (c === undefined) {
-        c = 0;
-    }
+    c = typeof c !== 'undefined' ? c : 0;
     return parseInt(a) + parseInt(b) + parseInt(c);
 };
