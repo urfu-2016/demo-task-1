9921b4f8d222537f992351d490984865a7c16c22
Малахов Александр <ySunural@yandex.ru>
2016-10-10 15:44:04 +0600

    Сумма произвольного кол-ва аргументов

--- math.js
+++ math.js
@@ -4,4 +4,14 @@ exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
+    var summ = 0;
+    var part = 0;
+    for (var i = 0; i < arguments.length; i++) {
+
+        part = parseInt(arguments[i], 10);
+        if (!isNaN(part)) {
+            summ += part;
+        }
+    }
+    return summ;
 };
