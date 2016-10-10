f27db68b2eac1d490e1bc6881cb124b500704c3f
Малахов Александр <ySunural@yandex.ru>
2016-10-10 21:09:57 +0500

    Восстановлена сигнатура, код разбит на  2 блока

--- math.js
+++ math.js
@@ -2,17 +2,29 @@
 
 exports.isStar = true;
 
-exports.sum = function () {
+exports.sum = function (a, b) {
 
     // Реализуйте сложение в этой функции
     var summ = 0;
-    var part = 0;
-    for (var i = 0; i < arguments.length; i++) {
-        part = parseInt(arguments[i], 10);
-        if (!isNaN(part)) {
-            summ += part;
+    a = parseInt(a, 10);
+    b = parseInt(b, 10);
+
+    if (!isNaN(a) && !isNaN(b)) {
+        summ = a + b;
+
+        return summ;
+    }
+    else {
+        var part = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            part = parseInt(arguments[i], 10);
+            if (!isNaN(part)) {
+                summ += part;
+            }
+
+            return summ;
         }
     }
 
-    return summ;
+
 };
