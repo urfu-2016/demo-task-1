225d265f20aaa8468720acd3415521eb4e1f2e6d
Алексей Котельников <alexeykotelnikov4@gmail.com>
2016-10-11 01:22:30 +0500

    fixed lint warnings

--- math.js
+++ math.js
@@ -2,9 +2,10 @@
 
 exports.isStar = true;
 
-exports.sum = function (number1, number2, number3) {    
-	number1 = parseInt(number1);
-	number2 = parseInt(number2);
-	number3 = parseInt(number3);
-	return number1 + number2 + number3;
+exports.sum = function (number1, number2, number3) {
+    number1 = parseInt(number1);
+    number2 = parseInt(number2);
+    number3 = parseInt(number3);
+
+    return number1 + number2 + number3;
 };
