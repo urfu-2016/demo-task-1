33ddc44b5443973e5bfe63b349da6b139c9d74c8
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-11 23:36:02 +0500

    Сохранение массива аргументов в переменную

--- math.js
+++ math.js
@@ -4,5 +4,6 @@ exports.isStar = true;
 
 exports.sum = function () {
     // Реализуйте сложение в этой функции
-    return Array.prototype.slice.call(arguments).reduce((acc, item) => acc + item);
+    var args = Array.prototype.slice.call(arguments);
+    return args.reduce((acc, item) => acc + item);
 };
