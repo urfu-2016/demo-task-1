0d2626b02da86c70a132f1bc46e36828d13967f8
Мамонов Антон <graf.M.96@yandex.ru>
2016-10-12 01:17:03 +0500

    Вторая попытка

--- math.js
+++ math.js
@@ -2,7 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
+exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
-    return parseFloat(a) + parseFloat(b);
+    return parseFloat(a) + parseFloat(b) + parseFloat(c);
 };
