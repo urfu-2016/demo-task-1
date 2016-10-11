001dc7ad29fdf4c6e96945e92f160e38804ed407
Pogromistka <int.sai@yandex.by>
2016-10-11 22:54:00 +0500

    Добавлен вариант решения демо-задачи 1

--- math.js
+++ math.js
@@ -4,4 +4,13 @@ exports.isStar = true;
 
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
+// приводим к типу number, если не NaN
+    if ((isNaN(a * 1) == false) && (isNaN(b * 1) == false)) {
+        a = a * 1;
+        b = b * 1;
+        return a + b;
+    }
+    else {
+        return "Введены неверные данные.";
+    }
 };
