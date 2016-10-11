bf765b9231c1269ebb5a266dfc3bfbbdac804668
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-11 23:55:33 +0500

    Линтинг

--- math.js
+++ math.js
@@ -4,8 +4,9 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
-    if (typeof(c) == "undefined"){
-        c = "";
+    if (typeof(c) === 'undefined') {
+        c = '';
     }
+
     return a + b + c;
 };
