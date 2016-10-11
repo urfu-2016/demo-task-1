ee05e3a4ee0a55d0db6a5ab6bf75b58222150ad8
Ян Назмиев <yannazmiev@gmail.com>
2016-10-11 16:48:05 +0500

    Третий вариант решения
    Добавлена конверсия типов

--- math.js
+++ math.js
@@ -3,9 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    return a + b;
+    return (+a) + (+b);
 };
 
 exports.sum = function (a, b, c) {
-    return a + b + c;
+    return (+a) + (+b) + (+c);
 };
