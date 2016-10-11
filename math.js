5b88c3e11f572cfaca47979ecf31959d66c079be
Pogromistka <int.sai@yandex.by>
2016-10-11 23:17:06 +0500

    Добавлен вариант решения демо-задачи 1 (испр.)

--- math.js
+++ math.js
@@ -5,9 +5,9 @@ exports.isStar = true;
 exports.sum = function (a, b) {
     // Реализуйте сложение в этой функции
 // приводим к типу number, если не NaN
-    if ((isNaN(a * 1) == false) && (isNaN(b * 1) == false)) {
-        a = a * 1;
-        b = b * 1;
+    if ((isNaN(Number(a)) == false) && (isNaN(Number(b)) == false)) {
+        a = Number(a);
+        b = Number(b);
         return a + b;
     }
     else {
