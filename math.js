62d0bdbfa8db2c0c6e878cd32475c3c74358c6ab
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-11 23:57:48 +0500

    Окей, по простому

--- math.js
+++ math.js
@@ -5,7 +5,7 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
     if (typeof(c) === 'undefined') {
-        c = '';
+        return a + b;
     }
 
     return a + b + c;
