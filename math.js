56675125a6dd1c18abc638abc3fc30a3e2abbec6
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-11 23:28:49 +0500

    Сложение произвольного числа аргументов

--- math.js
+++ math.js
@@ -2,7 +2,7 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции, окей
-    return a + b;
+exports.sum = function () {
+    // Реализуйте сложение в этой функции
+    return Array.prototype.slice.call(arguments).reduce((acc, item) => acc + item);
 };
