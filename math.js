1da6ecf124ef048d59bf6893a78ad8bf76939e67
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-11 23:49:35 +0500

    Ещё одна попытка пройти тест

--- math.js
+++ math.js
@@ -4,6 +4,5 @@ exports.isStar = true;
 
 exports.sum = function () {
     // Реализуйте сложение в этой функции
-    var args = Array.prototype.slice.call(arguments);
-    return args.reduce((acc, item) => acc + item);
+    return Array.prototype.reduce.call(arguments, (acc, item) => acc + item);
 };
