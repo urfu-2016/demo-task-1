c6b62ed6203df62049936e99c7a3abbcc0a6c86b
Mukhametov Denis <denis.mukhametov@gmail.com>
2016-10-12 00:05:07 +0500

    Приведение к числовому типу

--- math.js
+++ math.js
@@ -4,9 +4,12 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
+    a = Number(a);
+    b = Number(b);
     if (typeof(c) === 'undefined') {
         return a + b;
     }
+    c = Number(c);
 
     return a + b + c;
 };
