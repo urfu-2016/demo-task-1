f33a7c9716914e61c2faf1982b645945b7c9edc2
Sam Stikhin <ctishok@gmail.com>
2016-10-12 15:46:04 +0600

    5

--- math.js
+++ math.js
@@ -3,9 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (first, second, third) {
-    if (third === undefined)
-	    return first + second;
+    if (third === undefined) {
+        return first + second;
+    }
+
     return first + second + third;
-};
-
-
+}
