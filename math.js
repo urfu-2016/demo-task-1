275e7eb3166d0d5e25003b30ce8a9b714868798f
Pogromistka <int.sai@yandex.by>
2016-10-12 20:52:23 +0500

    Добавлен вариант решения демо-задачи 1 (испр. 6)

--- math.js
+++ math.js
@@ -6,13 +6,9 @@ exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
 // раз третий параметр не обязательный, его проверяем отдельно
 // если он не существует или неподходящего типа, то присваиваем 0
-    if (c !== undefined && isNaN(Number(c)) === false) {
-        c = Number(c);
-    }
-    else {
-        c = 0;
-    }
-    
+    if (c !== undefined && isNaN(Number(c)) === false) c = Number(c);
+    else c = 0;
+
 // приводим к типу number, если не NaN
     if ((isNaN(Number(a)) === false) && (isNaN(Number(b)) === false)) {
         a = Number(a);
