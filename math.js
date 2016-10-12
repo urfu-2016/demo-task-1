794a0cbe1f23ca860fa0a7f4678f460809704cd8
Мамонов Антон <graf.M.96@yandex.ru>
2016-10-12 22:32:07 +0500

    best version

--- math.js
+++ math.js
@@ -4,10 +4,9 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
-    var sum = 0;
-    for (var i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
+    if (arguments.length === 2) {
+        return parseFloat(a) + parseFloat(b);
+    } else if (arguments.length === 3) {
+        return parseFloat(a) + parseFloat(b) + parseFloat(c);
     }
-
-    return sum;
 };
