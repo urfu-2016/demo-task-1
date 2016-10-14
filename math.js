002321f5e2d8d007ae79a3a2621ea8f34ec4d9ed
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-15 01:09:10 +0500

    Похоже ясно

--- math.js
+++ math.js
@@ -1,10 +1,8 @@
 'use strict';
 
-exports.isStar = true;
+exports.isStar = false;
 
 exports.sum = function (a, b) {
     return Number(a) + Number(b);
 };
-exports.sum = function (a, b, c) {
-    return Number(a) + Number(b) + Number(c);
-};
+
