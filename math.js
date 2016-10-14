eb1a118982af0f930216e48e1402a17b5f969256
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-15 00:51:52 +0500

    Мои исправления

--- math.js
+++ math.js
@@ -2,16 +2,6 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b)
- {
-	if ( typeof a != 'number')
-	{
-		a = Number(a);
-	}
-	if ( typeof b != 'number')
-	{
-		b = Number(b);
-	}
-	
-	return a + b;
+exports.sum = function (a, b){
+	return Number(a) + Number(b);
 };
