3b94103a18274dbbff2044833fb95bf15aa983ea
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-15 00:03:39 +0500

    Сложение двух чисел

--- math.js
+++ math.js
@@ -3,5 +3,11 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
+	return (a+b);
+    // Реализуйте сложение в этой функции
+};
+
+exports.sum = function (a, b, c) {
+	return (a+b+c);
     // Реализуйте сложение в этой функции
 };
