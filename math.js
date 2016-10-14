deaf33a70f2119d696a2e8827cd84ecb1fd089ff
SergeyIstomin96 <sergeyistomin96bello@gmail.com>
2016-10-15 00:37:30 +0500

    Мои исправления

--- math.js
+++ math.js
@@ -1,9 +1,9 @@
 'use strict';
 
-exports.isStar = false;
+exports.isStar = true;
 
 exports.sum = function (a, b) {
-	var result = 0;
-	result = a + b;
-	return result;
+	var a2 = 0, b2 = 0;
+	a2 = a; b2 = b;
+	return a2 + b2;
 };
