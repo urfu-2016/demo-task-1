84684c25fec4eb0cb4293c9e46d68a6c1f05e7c2
Natalia Aksenova <dotokoto@gmail.com>
2016-10-25 19:46:49 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,13 +3,7 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    var now = Date.now();
-    var count = 0;
-    while (Date.now() - now < 1800) {
-        count = 0;
-    }
-
-    return Number(a) + Number(b) + Number(count);
+    return Number(a) + Number(b);
 };
 
 exports.whileTrue = function () {
