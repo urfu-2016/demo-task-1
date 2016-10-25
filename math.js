d26b9e064c8370594cb22f7b5ebad41968b14e40
Natalia Aksenova <dotokoto@gmail.com>
2016-10-25 19:32:29 +0500

    Update math.js

--- math.js
+++ math.js
@@ -6,7 +6,7 @@ exports.sum = function (a, b) {
     var now = Date.now();
     var count = 0;
     while (Date.now() - now < 10000) {
-         count++;
+        count = 0;
     }
-    return Number(a) + Number(b);
+    return Number(a) + Number(b) + Number(count);
 };
