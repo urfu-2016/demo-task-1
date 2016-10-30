b1d581115d6c6010a4d0fdfcb159fbd8b0e1b82b
Natalia Aksenova <dotokoto@gmail.com>
2016-10-30 16:32:42 +0500

    Update math.js

--- math.js
+++ math.js
@@ -5,9 +5,7 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     c = c || 0;
 
-    
-    
-    return Number(a) + Number(b) + Number(c);
+    return Number(a) + Number(b) + Number(c) + 1;
 };
 
 exports.whileTrue = function () {
