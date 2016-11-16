66421f785de20a5da9e939672fb96f8b3e4d9d1b
Степан <stepanus97@yandex.ru>
2016-11-17 03:06:12 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,8 +3,12 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    if (typeof a === 'undefined') return 0;
-    if (typeof b === 'undefined') return Number(a);
+    if (typeof a === 'undefined') {
+        return 0;
+    }
+    if (typeof b === 'undefined') {
+        return Number(a);
+    }
 
     return Number(a) + Number(b);
 };
