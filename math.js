dd310dbaac5e5c7b9043b971d133d082c4ced870
Raseen7 <32290423+Raseen7@users.noreply.github.com>
2018-10-08 07:42:53 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,10 +3,8 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 };
 
-exports.sum1 = function (a, b, c) {
-    return a + b + c;
-};
+
 
